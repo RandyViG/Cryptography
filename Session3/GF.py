37f0a1ce294c2b0a96b130c71fa896c88c92efd0
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-03-29 00:05:20 -0600

    Considerando todos los casos para 2^n

--- Session3/GF.py
+++ Session3/GF.py
@@ -1,19 +1,20 @@
 
 def calculateTable( n ):
     #polynomialsI = [ [3,1,0],[4,1,0],[5,2,0],[6,1,0],[7,1,0],[8,4,3,1,0] ]
-    polynomialsI = [ 10, 18 , 36 , 66 , 130 , 282 ]
+    polynomialsI = [ 11 , 19 , 37 , 67 , 131 , 283 ]
     module = polynomialsI[ n - 3 ]
-    for i in range( n ): 
-        valuesArchived = calculateValues( 2**i , n , module )
-        print(  valuesArchived )
-        #for j in range( 1 , ( 2 ** n ) ):
+    valuesArchived = [ ]
+    for i in range( 1, 2**n ): 
+        valuesArchived.append( calculateValues( i , n , module ) )
+    print(  valuesArchived )
+    #for j in range( 1 , ( 2 ** n ) ):
+
     
     return
 
 
 def calculateValues( i , n , module ):
     values = [ ]
-    print("Madulo: {}".format(module) )
     prev = i
     values.append( prev )
     for j in range( 2 , n+1 ):
